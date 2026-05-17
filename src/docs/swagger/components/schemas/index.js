e579ea9599f17376d220b606ffddf578e290c927
Zaxzz <hasyahhamm@gmail.com>
2026-05-17 19:35:38 +0700

    add swagger

--- src/docs/swagger/components/schemas/index.js
+++ src/docs/swagger/components/schemas/index.js
@@ -2,10 +2,14 @@ const sharedSchemas = require("./shared");
 const userSchemas = require("./user");
 const trackingSchemas = require("./tracking");
 const tokoSchemas = require("./toko");
+const dashboardSchemas = require("./dashboard");
+const manajemenLayananSchemas = require("./manajemen_layanan");
 
 module.exports = {
   ...sharedSchemas,
   ...userSchemas,
   ...trackingSchemas,
   ...tokoSchemas,
+  ...dashboardSchemas,
+  ...manajemenLayananSchemas,
 };
--- src/docs/swagger/paths/index.js
+++ src/docs/swagger/paths/index.js
@@ -1,9 +1,13 @@
 const userAuthPaths = require("./user/auth");
 const adminTrackingPaths = require("./admin/tracking");
 const adminTokoPaths = require("./admin/toko");
+const adminDashboardPaths = require("./admin/dashboard");
+const adminManajemenLayananPaths = require("./admin/manajemen_layanan");
 
 module.exports = {
   ...userAuthPaths,
   ...adminTrackingPaths,
   ...adminTokoPaths,
+  ...adminDashboardPaths,
+  ...adminManajemenLayananPaths,
 };
