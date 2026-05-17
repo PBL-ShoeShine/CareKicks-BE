f3600895ca2ba68ad0afb0573d9a0a7ffead5099
Ziannnniiii <haryoraul@gmail.com>
2026-05-17 19:07:05 +0700

    feat: add api doc inventaris

--- src/docs/swagger/components/schemas/index.js
+++ src/docs/swagger/components/schemas/index.js
@@ -2,10 +2,12 @@ const sharedSchemas = require("./shared");
 const userSchemas = require("./user");
 const trackingSchemas = require("./tracking");
 const tokoSchemas = require("./toko");
+const inventarisSchemas = require("./inventaris");
 
 module.exports = {
   ...sharedSchemas,
   ...userSchemas,
   ...trackingSchemas,
   ...tokoSchemas,
+  ...inventarisSchemas,
 };
--- src/docs/swagger/paths/index.js
+++ src/docs/swagger/paths/index.js
@@ -1,9 +1,11 @@
 const userAuthPaths = require("./user/auth");
 const adminTrackingPaths = require("./admin/tracking");
 const adminTokoPaths = require("./admin/toko");
+const adminInventarisPaths = require("./admin/inventaris");
 
 module.exports = {
   ...userAuthPaths,
   ...adminTrackingPaths,
   ...adminTokoPaths,
+  ...adminInventarisPaths,
 };
