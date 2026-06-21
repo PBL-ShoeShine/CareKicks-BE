1890aa9e979edc89690d7a99f2374fcefe8abfc5
Anisa <dailyfn93@gmail.com>
2026-06-21 16:25:46 +0700

    feat: menambahkan fitur lupa password pada login, menghapus icon notification pada scanner

--- src/features/auth/auth.routes.js
+++ src/features/auth/auth.routes.js
@@ -4,11 +4,16 @@ const router = express.Router();
 // PATH YANG BENAR SESUAI STRUKTUR FOLDERMU:
 // Mundur ke features (../) -> masuk ke admin -> masuk ke edit_profile
 const editProfileController = require("../admin/edit_profile/edit_profile.controller");
+const forgotPasswordController = require('./forgot_password.controller');
 
 // =====================================================================
 // RUTE VERIFIKASI EMAIL (Diakses dari link di Email)
 // TIDAK BOLEH pakai authMiddleware karena dipanggil via browser biasa
 // =====================================================================
 router.get("/verify-email", editProfileController.verifyEmail);
+// RUTE LUPA PASSWORD (PUBLIK - TANPA MIDDLEWARE TOKEN)
+router.post('/forgot-password/request-otp', forgotPasswordController.requestOtp);
+router.post('/forgot-password/verify-otp', forgotPasswordController.verifyOtp);
+router.put('/forgot-password/reset', forgotPasswordController.resetPassword);
 
 module.exports = router;
